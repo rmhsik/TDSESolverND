6f3464136e3a14dff1a9a468d07818fa4520d857
Rodrigo Martin Hernandez <rodrigomh@usal.es>
2022-01-28 20:05:49 +0100

    Diagnostic for only one type

--- include/diagnostics.h
+++ include/diagnostics.h
@@ -1,21 +1,31 @@
 #ifndef DIAGNOSTICS_H
 #define DIAGNOSTICS_H
 
+#include <string>
 #include "wavefunction.h"
 #include "hamiltonian.h"
 #include "parameters.h"
 
+#define ACC_I 0
+#define ACC_K 1
+#define DIP_I 2
+#define DIP_K 3
+#define POP   4
+
 class Diagnostics{
     protected:
         Parameters *_param;
-        cdouble *_acc_i, *_acc_k;
-        cdouble *_dip_i, *_dip_k;
-        cdouble *_pop;
-     
+        cdouble *_data;
+        
+        std::sttring _def;
+        int _type;
+        std::string _data_path;
+        double _int_imin, _int_imax;
+        double _int_kmin, _int kmax;
+
         int _ni, _nk;
         double _di, _dk; 
         double *_i, *_k;
-
         Hamiltonian *_ham;
         WF *_wf;
         virtual void _acc_i_buf(const int idx){};
@@ -23,18 +33,17 @@ class Diagnostics{
         virtual void _dip_i_buf(const int idx){};
         virtual void _dip_k_buf(const int idx){};
         virtual void _pop_buf(const int idx){};
+
+        void _parse_def();
     public:
         Diagnostics();
-        Diagnostics(Parameters * param);
+        Diagnostics(std::string def);
+        void set_parameters(Parameters *param);
         void set_geometry(double *i, double *k, const double di, const double dk);
         void set_ham(Hamiltonian *ham);
         void set_wf(WF *wf); 
         void run_diagnostics(const int idx);
-        cdouble* get_acc_i();
-        cdouble* get_acc_k();
-        cdouble* get_dip_i();
-        cdouble* get_dip_k();
-        cdouble* get_pop();
+        cdouble* get_data();
         void write_diagnostics();
 };
 
--- src/diagnostics.cpp
+++ src/diagnostics.cpp
@@ -6,13 +6,14 @@ Diagnostics::Diagnostics(){
 
 }
 
-Diagnostics::Diagnostics(Parameters *param){
-   _param = param; 
-   _acc_i = new cdouble[_param->nt];
-   _acc_k = new cdouble[_param->nt];
-   _dip_i = new cdouble[_param->nt];
-   _dip_k = new cdouble[_param->nt];
-   _pop   = new cdouble[_param->nt];
+Diagnostics::Diagnostics(std::string def){
+    _def = def;
+   }
+
+void Diagnostics::set_parameters(Parameters *param){
+    _param = param;
+    _data = new cdouble[_param->nt];
+
 }
 
 void Diagnostics::set_geometry(double *i, double *k, const double di, const double dk){
@@ -21,32 +22,22 @@ void Diagnostics::set_geometry(double *i, double *k, const double di, const doub
    _nk = _param->nk;
 }
 
-cdouble* Diagnostics::get_acc_i(){
-    return _acc_i;
-}
-
-cdouble* Diagnostics::get_acc_k(){
-    return _acc_k;
-}
-
-cdouble* Diagnostics::get_dip_i(){
-    return _dip_i;
-}
-
-cdouble* Diagnostics::get_dip_k(){
-    return _dip_k;
-}
-
-cdouble* Diagnostics::get_pop(){
-    return _pop;
+cdouble* Diagnostics::get_data(){
+    return _data;
 }
 
 void Diagnostics::write_diagnostics(){
-    write_array(_acc_i, _param->nt, _param->acc_i_path);
-    write_array(_acc_k, _param->nt, _param->acc_k_path);
-    write_array(_dip_i, _param->nt, _param->dip_i_path);
-    write_array(_dip_k, _param->nt, _param->dip_k_path);
-    write_array(_pop,   _param->nt, _param->pop_path);
+    write_array(_data, _param->nt, _data_path);
 }
 
-
+void Diagnostics::_parse_def(){
+    char delim = ",";
+    std::stringstream ss(_def);
+    std::string item;
+    std::vector<std::string> parsed;
+    while(getline(ss,item,delim)){
+        parsed.push_back(item);
+    }
+    _
+    
+}
