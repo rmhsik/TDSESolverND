13459b3787f388d6604dc0d96ab63f180e4be2dd
Rodrigo Martín Hernández <rodrigomh@usal.es>
2022-02-02 14:22:54 +0100

    Geometry 2 for electric and magnetic field

--- src/potential.cpp
+++ src/potential.cpp
@@ -2,10 +2,15 @@
 #include "potential.h"
 
 
-cdouble potential( double i, double k, double t, Hamiltonian *ham){
-    cdouble omegaB = ham->_param->B0k/(sqrt(8));
-    cdouble omega0 = ham->_param->w0;
-    return 0.5*omegaB*omegaB*i*i - 0.5*omegaB*omegaB*i*i*cos(2.0*omega0*t);
+//cdouble potential( double i, double k, double t, Hamiltonian *ham){
+//    cdouble omegaB = ham->_param->B0k/(sqrt(8));
+//    cdouble omega0 = ham->_param->w0;
+//    return 0.5*omegaB*omegaB*i*i - 0.5*omegaB*omegaB*i*i*cos(2.0*omega0*t);
+//}
+
+cdouble potential(double i, double k, double t, Hamiltonian *ham){
+    double bfield_i = ham->_param->B0i;
+    return -1.0/sqrt(i*i+k*k + 0.65) + 0.125*bfield_i*bfield_i*(i*i+k*k);
 }
 
 cdouble potential_X( double i, double k, double ti, Hamiltonian *ham){
