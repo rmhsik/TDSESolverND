6cb580fff19688b8050afc2dbafbcbbcd694abf9
Rodrigo Martín Hernández <rodrigomh@usal.es>
2022-02-02 09:40:15 +0100

    Use pontential function in Hamiltonian X

--- src/hamiltonian_X.cpp
+++ src/hamiltonian_X.cpp
@@ -44,7 +44,7 @@ void Hamiltonian::step_i_X(cdouble *psi, const int j, const int ti, const int im
     cdouble b = 1.0/(2.0*_di*_di); 
     for(int i=0;i<_ni;i++){
         H_du = -b + I*1.0/(2.0*C*_di)*afield_i;
-        H_d  =  a + _potential[i*_nk + 0] + 0.5*afield_i*afield_i/(C*C);
+        H_d  =  a + _potential_fn(_i[i],0,0) + 0.5*afield_i*afield_i/(C*C);
         H_dl = -b - I*1.0/(2.0*C*_di)*afield_i;
 
         _Mi_du[i] = I*H_du*dt/2.0;
@@ -78,7 +78,7 @@ cdouble Hamiltonian::ener_X(cdouble *psi){
     
     for(int i=0;i<_ni;i++){
         H_du[i] = -b;
-        H_d[i]  =  a + _potential[i*_nk + 0];
+        H_d[i]  =  a + _potential_fn(_i[i],0,0);
         H_dl[i] = -b;
         row[i] = psi[i*_nk + 0];
     }
--- src/potential.cpp
+++ src/potential.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
 #include "potential.h"
 
-#define A 0.65
+#define A 2.0
 
 cdouble potential( double i, double k, int ti, Hamiltonian *ham){
     return -1.0/sqrt(i*i + k*k + A);
